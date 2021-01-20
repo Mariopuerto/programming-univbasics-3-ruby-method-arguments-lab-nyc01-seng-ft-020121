b81fcc1a4cc91f5c60690d25f3bf53406ac980d8
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-20 03:02:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(name, language)
+def introduction_with_language(name = "john", language)
    puts "Hi, my name is #{name} and i am learning to program in #{language}."
 end
 
@@ -11,7 +11,7 @@ def introduction_with_language_optional(name, language = "Ruby")
 end
 
 
-introduction_with_language("john", "language")
+introduction_with_language("name", "language")
 introduction_with_language_optional("name", "language")
 
 
